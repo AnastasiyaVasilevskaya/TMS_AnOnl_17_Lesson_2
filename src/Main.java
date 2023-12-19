b8564eaad18ba410a51e8bb2034825853b76ee38
Anastasiya Vasilevskaya <nastena2004vasil@gmail.com>
2023-12-19 15:22:41 +0300

    add lesson_1 code in lesson_2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        // 1 задание
+        System.out.println("Введите два числа: ");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println("Сумма чисел: " + (a + b));
+        System.out.println();
+
+        // 2 задание
+        System.out.println("Hello World");
+        System.out.println();
+
+        // 3 задание
+        System.out.println("Введите Ваше имя: ");
+        String user = scan.next();
+        System.out.println("Hello " + user);
+
+
     }
 }
